ddc2856b05f75fa257c282fc93ba1143a4b6a1a1
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:23:19 +0100

    Adds failing test for checkout to initalize with prices"

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,3 +1,10 @@
 describe Checkout do
-  
+
+  let(:co) { described_class.new }
+
+  describe '#defaults' do
+    it'should initialize with a list of items' do
+      expect(co.prices).to eq({001 => 9.25, 002 => 45.00, 003 => 19.95})
+    end
+  end
 end
