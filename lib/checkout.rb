6cac20121091e1262cda0972a2c7e708a60d445a
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:32:52 +0100

    Adds failing test for calculating total

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -3,9 +3,9 @@ class Checkout
   attr_reader :prices, :basket
 
   def initialize
-    @prices = { 001 => 9.25,
-                002 => 45.00,
-                003 => 19.95
+    @prices = { '001' => 9.25,
+                '002' => 45.00,
+                '003' => 19.95
               }
     @basket = []
   end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,14 +4,28 @@ describe Checkout do
 
   describe 'defaults' do
     it 'should initialize with a list of items' do
-      expect(co.prices).to eq({001 => 9.25, 002 => 45.00, 003 => 19.95})
+      expect(co.prices).to eq({'001' => 9.25, '002' => 45.00, '003' => 19.95})
     end
   end
 
   describe '#scan' do
     it 'should add a scanned item number to the basket' do
-      co.scan(001)
-      expect(co.basket).to eq([001])
+      co.scan('001')
+      expect(co.basket).to eq(['001'])
+    end
+    it 'should add multiple items to the basket' do
+      co.scan('001')
+      co.scan('002')
+      co.scan('001')
+      expect(co.basket).to eq(['001', '002', '001'])
+    end
+  end
+
+  describe '#total' do
+    it 'should calculate the cost of the basket' do
+      co.scan('001')
+      co.scan('002')
+      expect(co.total).to eq(54.25)
     end
   end
 
