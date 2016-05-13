5d1d387f39a036a201582f0267ed1fa246b85054
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:24:41 +0100

    Adds prices to checkout:

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,12 @@
 class Checkout
 
+  attr_reader :prices
+
+  def initialize
+    @prices = { 001 => 9.25,
+                002 => 45.00,
+                003 => 19.95
+              }
+  end
+
 end
