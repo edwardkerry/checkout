a6c4251f4abcaceb5477af1f77cb08802bca1748
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:13:05 +0100

    Adds all failing feature tests

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,4 +1,4 @@
-describe 'the checkout' do
+describe 'Feature test' do
 
   context 'end to end feature testing' do
     describe 'basket one' do
@@ -10,6 +10,25 @@ describe 'the checkout' do
         expect(co.total).to equal(66.78)
       end
     end
+    describe 'basket two' do
+      it 'should cost 36.95' do
+        co = Checkout.new
+        co.scan('001')
+        co.scan('003')
+        co.scan('001')
+        expect(co.total).to equal(36.95)
+      end
+    end
+    describe 'basket three' do
+      it 'should cost 73.76' do
+        co = Checkout.new
+        co.scan('001')
+        co.scan('002')
+        co.scan('001')
+        co.scan('003')
+        expect(co.total).to equal(73.76)
+      end
+    end
   end
-  
+
 end
