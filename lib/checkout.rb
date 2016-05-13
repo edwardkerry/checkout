64a9c972f04a15d69a5b016901a0fb7ccc8e59c5
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:27:30 +0100

    Adds scan and basket function to checkout

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,12 +1,17 @@
 class Checkout
 
-  attr_reader :prices
+  attr_reader :prices, :basket
 
   def initialize
     @prices = { 001 => 9.25,
                 002 => 45.00,
                 003 => 19.95
               }
+    @basket = []
+  end
+
+  def scan(item)
+    @basket << item
   end
 
 end
