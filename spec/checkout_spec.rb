432fde8df648b75221496cc8481b11aa218c62f4
Ed Kerry <edwardkerry@gmail.com>
2016-05-13 16:26:31 +0100

    Adds failing test for scan

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,9 +2,17 @@ describe Checkout do
 
   let(:co) { described_class.new }
 
-  describe '#defaults' do
-    it'should initialize with a list of items' do
+  describe 'defaults' do
+    it 'should initialize with a list of items' do
       expect(co.prices).to eq({001 => 9.25, 002 => 45.00, 003 => 19.95})
     end
   end
+
+  describe '#scan' do
+    it 'should add a scanned item number to the basket' do
+      co.scan(001)
+      expect(co.basket).to eq([001])
+    end
+  end
+
 end
