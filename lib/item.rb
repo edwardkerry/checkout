c36b2a5e71aca11255cd44fdd33fbee68d5b7ced
Ed Kerry <edwardkerry@gmail.com>
2016-05-15 13:57:08 +0100

    Adds Item class and initialize spec)

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,11 @@
 class Item
+
+  attr_reader :code, :name, :price, :catalogue
+
+  def initialize(code,name,price)
+    @code = code
+    @name = name
+    @price = price.to_i
+  end
+
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,19 @@
 describe Item do
 
+  let(:item1) { described_class.new('001', 'Travel Card Holder', '925')}
+
+  describe 'defaults' do
+    it 'should give each item a code' do
+      expect(item1.code).to eq('001')
+    end
+    it 'should give each item a name' do
+      expect(item1.name).to eq('Travel Card Holder')
+    end
+    it 'should give each item a price' do
+      expect(item1.price).to eq(925)
+    end
+  end
+
+
 
 end
