4444208312b7e46af0b77a62cab327806b29c5ae
Ed Kerry <edwardkerry@gmail.com>
2016-05-16 14:39:30 +0100

    Adds spec and class for multibuy promotion

--- lib/multibuy_discount.rb
+++ lib/multibuy_discount.rb
@@ -8,4 +8,15 @@ class MultibuyDiscount
     @discount_price = discount_price
   end
 
+  def apply_discount(order)
+    return calculate_discount(order) if order[@code] >= @quantity
+    return 0
+  end
+
+  private
+
+  def calculate_discount(order)
+    order[@code] * @discount_price
+  end
+
 end
--- spec/multibuy_spec.rb
+++ spec/multibuy_spec.rb
@@ -1,16 +1,28 @@
 describe MultibuyDiscount do
 
-    let(:discount) { described_class.new('001', 2, 850) }
+  let(:discount) { described_class.new('001', 2, 75) }
+  let(:order1) { {'001'=>2, '002'=>1} }
+  let(:order2) { {'001'=>1, '002'=>3} }
 
-    describe 'defaults' do
-      it 'sets the item code' do
-        expect(discount.code).to eq ('001')
-      end
-      it 'sets the item quantity' do
-        expect(discount.quantity).to eq (2)
-      end
-      it 'sets the discounted price' do
-        expect(discount.discount_price).to eq (850)
-      end
+  describe 'defaults' do
+    it 'sets the item code' do
+      expect(discount.code).to eq ('001')
     end
+    it 'sets the item quantity' do
+      expect(discount.quantity).to eq(2)
+    end
+    it 'sets the discounted price' do
+      expect(discount.discount_price).to eq(75)
+    end
+  end
+
+  describe '#apply_discount' do
+    it 'calculates a discount on multi-buy items' do
+      expect(discount.apply_discount(order1)).to eq(150)
+    end
+    it 'does not offer discounts on invalid items' do
+      expect(discount.apply_discount(order2)).to eq(0)
+    end
+  end
+
 end
